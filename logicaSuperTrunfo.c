99a8f75b76ee2617207d94e25aae0485064a9614
betyzi <b3atrizmach4do09@gmail.com>
2025-06-07 16:05:47 +0000

    fiz a primeira parte da logica do jogo, comparação

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -2,42 +2,89 @@
 
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
 
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+//Primeiro começo definindo as veriáveis do codigo para armazenar os dados das cartas.
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    // Variáveis para a Carta 1
+    char estado1;
+    char codigo1[5];
+    char nomecidade1[50];
+    unsigned long int populacao1;
+    float area1, pib1;
+    int pontosturisticos1;
+    float densidade1, pibpercapita1;
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+   // Variáveis para a Carta 2
+    char estado2;
+    char codigo2[5];
+    char nomecidade2[50];
+    unsigned long int populacao2;
+    float area2, pib2;
+    int pontosturisticos2;
+    float densidade2, pibpercapita2;
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+// Depois uso as funções certas para a entrada de dados das cartas.
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+     // Pegando os ndados da carata 1
+    printf("Cadastro da Carta 1\n");
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado1);
+    printf("Código (ex: A01): ");
+    scanf("%s", codigo1);
+    printf("Nome da cidade: ");
+    scanf(" %[^\n]", nomecidade1);
+    printf("População: ");
+    scanf("%lu", &populacao1);
+    printf("Área (km²): ");
+    scanf("%f", &area1);
+    printf("PIB (bilhões): ");
+    scanf("%f", &pib1);
+    printf("Pontos turísticos: ");
+    scanf("%d", &pontosturisticos1);
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+
+     // Pegando os ndados da carata 2
+    printf("\nCadastro da Carta 2\n");
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado2);
+    printf("Código (ex: B03): ");
+    scanf("%s", codigo2);
+    printf("Nome da cidade: ");
+    scanf(" %[^\n]", nomecidade2);
+    printf("População: ");
+    scanf("%lu", &populacao2);
+    printf("Área (km²): ");
+    scanf("%f", &area2);
+    printf("PIB (bilhões): ");
+    scanf("%f", &pib2);
+    printf("Pontos turísticos: ");
+    scanf("%d", &pontosturisticos2);
+
+    // Agora os cálculos
+    densidade1 = populacao1 / area1;
+    pibpercapita1 = pib1 / populacao1;
+
+    densidade2 = populacao2 / area2;
+    pibpercapita2 = pib2 / populacao2;
+
+
+    // Comparação por População
+    printf("\n--- Comparação de cartas (Atributo: População) ---\n");
+    printf("Carta 1 - %s (%c): %lu habitantes\n", nomecidade1, estado1, populacao1);
+    printf("Carta 2 - %s (%c): %lu habitantes\n", nomecidade2, estado2, populacao2);
+
+    if (populacao1 > populacao2) {
+        printf("Resultado: Carta 1 (%s) venceu!\n", nomecidade1);
+    } else if (populacao2 > populacao1) {
+        printf("Resultado: Carta 2 (%s) venceu!\n", nomecidade2);
+    } else {
+        printf("Resultado: Empate!\n");
+    }
 
     return 0;
 }
+
+    
+    
